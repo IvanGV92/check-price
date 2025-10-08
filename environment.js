72b6e1153d439cf08605edd827527da88a0b5772
IvanGV92 <43556637+IvanGV92@users.noreply.github.com>
2025-10-07 22:26:38 -0500

    Update environment.js

--- environment.js
+++ environment.js
@@ -4,5 +4,6 @@ module.exports = {
   'http://184.72.143.25:8080/get-price',
   'http://13.222.134.75:8080/get-price',
   'http://54.163.208.239:8080/get-price'
-  ]
+  ],
+  VERIFY_SERVICE_URL: 'http://localhost:8081'
 };
